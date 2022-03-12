c124f2418dfcde6cc226a5b04ca548074fd8e748
MShamhalov <m@shamhalov.ru>
2022-03-12 23:35:24 +0300

    first commit 279.c

---
+++ 279.c
@@ -0,0 +1,11 @@
+unsigned long qe2(unsigned long x, unsigned long y, unsigned long n){
+    unsigned long s, t, u;
+    int i;
+    s = 1; t = x; u = y;
+    while (u){
+        if ( u&1 ) s = (s*t)%n;
+        u >>= 1;
+        t = (t*t)%n;
+    }
+    return(s);
+}
