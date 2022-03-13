d80b3e889b579a69156aa6035cea3a982a9a0da2
Magomed <magomed@shamhalov.ru>
2022-03-13 03:27:02 +0300

    little fix

--- 29.c
+++ 29.c
@@ -3,7 +3,7 @@ void main (int argc, char *argv[])
   FILE *fi, *fo;
   char *cp;
   int c;
-  
+
   if ((cp = argv[1]) && *cp!= '\0'){
     if ((fi = fopen(argv[2], "rb")) != NULL){
       if ((fo = fopen(argv[3], "wb")) != NULL){
